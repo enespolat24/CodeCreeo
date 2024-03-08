965eea4e785f97c7abc06cd4b2b51a1ea5d213fd
enespolat24 <enespolat24@gmail.com>
2024-03-08 18:36:56 +0300

    redis connection uses env variables if exists

--- database/redis.go
+++ database/redis.go
@@ -1,12 +1,35 @@
 package database
 
-import "github.com/redis/go-redis/v9"
+import (
+	"os"
+	"strconv"
+
+	"github.com/redis/go-redis/v9"
+)
 
 func StartRedis() *redis.Client {
-	redis := redis.NewClient(&redis.Options{
-		Addr:     "redis:6379",
-		Password: "", // no password set
-		DB:       0,  // use default DB
-	})
-	return redis
+	redisURL := os.Getenv("REDIS_URL")
+	if redisURL == "" {
+		redisURL = "localhost:6379"
+	}
+
+	redisPassword := os.Getenv("REDIS_PASSWORD")
+	redisDB := os.Getenv("REDIS_DB")
+
+	opt := &redis.Options{
+		Addr:     redisURL,
+		Password: redisPassword,
+		DB:       0, // Default value
+	}
+
+	if redisDB != "" {
+		db, err := strconv.Atoi(redisDB)
+		if err != nil {
+			panic(err)
+		}
+		opt.DB = db
+	}
+
+	redisClient := redis.NewClient(opt)
+	return redisClient
 }
