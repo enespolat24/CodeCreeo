c348a07250a20e6ff01d9fe47f4543767d3b17d6
enespolat24 <enespolat24@gmail.com>
2024-03-08 18:29:46 +0300

    redis connection

--- database/redis.go
+++ database/redis.go
@@ -1,5 +1,12 @@
 package database
 
-func NewRedisConnection() {
+import "github.com/redis/go-redis/v9"
 
+func StartRedis() *redis.Client {
+	redis := redis.NewClient(&redis.Options{
+		Addr:     "redis:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	return redis
 }
